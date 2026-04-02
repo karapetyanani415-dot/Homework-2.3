3464fde061b11800e4d74f501e608d99599678ad
karapetyanani415-dot <karapetyanani415@gmail.com>
2026-04-02 19:48:09 +0400

    Library practice

--- 28.03(practice/Library with Constructor functon.js
+++ 28.03(practice/Library with Constructor functon.js
@@ -1,91 +1,87 @@
 function Book(id, title, author, year) {
-    this.id = id
-    this.title = title
-    this.author = author
-    this.year = year
-    this.isAvailable = true
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.isAvailable = true;
 }
 
 function Library(name) {
-    this.name = name
-    this.books = []
+  this.name = name;
+  this.books = [];
 }
 
 Library.prototype.addBook = function (book) {
-    if (!book.id) {
-        throw new Error("do not add a book without id")
-    }
-    if (!book.title) {
-        throw new Error("do not add a book without title")
-    }
-    if (!book.author) {
-        throw new Error("do not add a book without author")
-    }
-    if (typeof book.year !== "number") {
-        throw new Error("year must be a number")
-    }
-    let id = book.id
-    let books = this.books
-    let unique = books.some(e => e.id === id)
-    if (unique) {
-        throw new Error("ID must be unique")
-    }
-    this.books.push(book)
-
-}
+  if (!book.id) {
+    throw new Error("do not add a book without id");
+  }
+  if (!book.title) {
+    throw new Error("do not add a book without title");
+  }
+  if (!book.author) {
+    throw new Error("do not add a book without author");
+  }
+  if (typeof book.year !== "number") {
+    throw new Error("year must be a number");
+  }
+  let id = book.id;
+  let books = this.books;
+  let unique = books.some((e) => e.id === id);
+  if (unique) {
+    throw new Error("ID must be unique");
+  }
+  this.books.push(book);
+};
 Library.prototype.removeBook = function (id) {
-    let books = this.books
-    let unique = books.some(e => e.id === id)
-    if (!unique) {
-        throw new Error("ID must be unique")
+  let books = this.books;
+  let unique = books.some((e) => e.id === id);
+  if (!unique) {
+    throw new Error("ID must be unique");
+  }
+  for (let i = 0; i < books.length; ++i) {
+    if (books[i].id === id) {
+      books[i] = null;
     }
-    for (let i = 0; i < books.length; ++i) {
-        if (books[i].id === id) {
-            books[i] = null
-        }
-    }
-}
+  }
+};
 Library.prototype.findBookByTitle = function (title) {
-    let book = this.books.find(e => e.title === title)
-    return book
-}
+  let book = this.books.find((e) => e.title === title);
+  return book;
+};
 Library.prototype.findBookByAuthor = function (author) {
-    return this.books.filter(b => b.author === author)
-}
+  return this.books.filter((b) => b.author === author);
+};
 Library.prototype.borrowBook = function (id) {
-    let book = this.books.find(e => e.id === id)
-    if (!book) {
-        throw new Error("Book not found");
-    }
-    if (!book.isAvailable) {
-        throw new Error("Book already borrowed");
-    }
-    book.isAvailable = false;
-}
+  let book = this.books.find((e) => e.id === id);
+  if (!book) {
+    throw new Error("Book not found");
+  }
+  if (!book.isAvailable) {
+    throw new Error("Book already borrowed");
+  }
+  book.isAvailable = false;
+};
 Library.prototype.returnBook = function (id) {
-    let book = this.books.find(e => e.id === id)
-    if (!book) {
-        throw new Error("Book not found");
-    }
-    if (book.isAvailable) {
-        throw new Error("Book already borrowed");
-    }
-    book.isAvailable = true;
-}
+  let book = this.books.find((e) => e.id === id);
+  if (!book) {
+    return "Book not found";
+  }
+  if (book.isAvailable) {
+    return "Book already borrowed";
+  }
+  book.isAvailable = true;
+};
 Library.prototype.listAvailableBooks = function () {
-    let book = this.books.filter(e => e.isAvailable)
-    return book
-}
+  let book = this.books.filter((e) => e.isAvailable);
+  return book;
+};
 Library.prototype.listBorrowedBooks = function () {
-    let book = this.books.filter(e => !e.isAvailable)
-    return book
-}
+  let book = this.books.filter((e) => !e.isAvailable);
+  return book;
+};
 Library.prototype.showLibraryInfo = function () {
-    console.log(this.name)
-    console.log(this.books.length)
-    let available = this.listAvailableBooks()
-    console.log(available.length)
-    let borrow = this.listBorrowedBooks()
-    console.log(borrow.length)
-
-}
+  console.log(`Library name: ${this.name}`);
+  console.log(`Total books: ${this.books.length}`);
+  console.log(`Available: ${this.listAvailableBooks().length}`);
+  console.log(`Borrowed: ${this.listBorrowedBooks().length}`);
+};
--- 28.03(practice/Library with class.js
+++ 28.03(practice/Library with class.js
@@ -1,91 +1,89 @@
 class Book {
-    constructor(id, title, author, year) {
-        this.id = id
-        this.title = title
-        this.author = author
-        this.year = year
-        this.isAvailable = true
-    }
+  constructor(id, title, author, year) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.isAvailable = true;
+  }
 }
 class Library {
-    constructor(name) {
-        this.name = name
-        this.books = []
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+  addBook(book) {
+    if (!book.id) {
+      throw new Error("do not add a book without id");
     }
-    addBook(book) {
-        if (!book.id) {
-            throw new Error("do not add a book without id")
-        }
-        if (!book.title) {
-            throw new Error("do not add a book without title")
-        }
-        if (!book.author) {
-            throw new Error("do not add a book without author")
-        }
-        if (typeof book.year !== "number") {
-            throw new Error("year must be a number")
-        }
-        let id = book.id
-        let books = this.books
-        let unique = books.some(e => e.id === id)
-        if (unique) {
-            throw new Error("ID must be unique")
-        }
-        this.books.push(book)
+    if (!book.title) {
+      throw new Error("do not add a book without title");
     }
-    removeBook(id) {
-        let books = this.books
-        let unique = books.some(e => e.id === id)
-        if (!unique) {
-            throw new Error("ID must be unique")
-        }
-        for (let i = 0; i < books.length; ++i) {
-            if (books[i].id === id) {
-                books[i] = null
-            }
-        }
+    if (!book.author) {
+      throw new Error("do not add a book without author");
     }
-    findBookByTitle(title) {
-        let book = this.books.find(e => e.title === title)
-        return book
+    if (typeof book.year !== "number") {
+      throw new Error("year must be a number");
     }
-    findBookByAuthor(author) {
-        return this.books.filter(b => b.author === author)
+    let id = book.id;
+    let books = this.books;
+    let unique = books.some((e) => e.id === id);
+    if (unique) {
+      throw new Error("ID must be unique");
     }
-    borrowBook(id) {
-        let book = this.books.find(e => e.id === id)
-        if (!book) {
-            throw new Error("Book not found");
-        }
-        if (!book.isAvailable) {
-            throw new Error("Book already borrowed");
-        }
-        book.isAvailable = false;
+    this.books.push(book);
+  }
+  removeBook(id) {
+    let books = this.books;
+    let unique = books.some((e) => e.id === id);
+    if (!unique) {
+      throw new Error("ID must be unique");
     }
-    returnBook(id) {
-        let book = this.books.find(e => e.id === id)
-        if (!book) {
-            throw new Error("Book not found");
-        }
-        if (book.isAvailable) {
-            throw new Error("Book already borrowed");
-        }
-        book.isAvailable = true;
+    for (let i = 0; i < books.length; ++i) {
+      if (books[i].id === id) {
+        books[i] = null;
+      }
     }
-    listAvailableBooks() {
-        let book = this.books.filter(e => e.isAvailable)
-        return book
+  }
+  findBookByTitle(title) {
+    let book = this.books.find((e) => e.title === title);
+    return book;
+  }
+  findBookByAuthor(author) {
+    return this.books.filter((b) => b.author === author);
+  }
+  borrowBook(id) {
+    let book = this.books.find((e) => e.id === id);
+    if (!book) {
+      throw new Error("Book not found");
     }
-    listBorrowedBooks() {
-        let book = this.books.filter(e => !e.isAvailable)
-        return book
+    if (!book.isAvailable) {
+      throw new Error("Book already borrowed");
     }
-    showLibraryInfo() {
-        console.log(`library name : ${this.name}`)
-        console.log(`total number of books : ${this.books.length}`)
-        let available = this.listAvailableBooks()
-        console.log(`number of available books : ${available.length}`)
-        let borrow = this.listBorrowedBooks()
-        console.log(`number of borrowed books : ${borrow.length}`)
+    book.isAvailable = false;
+  }
+  returnBook(id) {
+    let book = this.books.find((e) => e.id === id);
+    if (!book) {
+      return "Book not found";
     }
+    if (book.isAvailable) {
+      return "Book already borrowed";
+    }
+    book.isAvailable = true;
+  }
+  listAvailableBooks() {
+    let book = this.books.filter((e) => e.isAvailable);
+    return book;
+  }
+  listBorrowedBooks() {
+    let book = this.books.filter((e) => !e.isAvailable);
+    return book;
+  }
+  showLibraryInfo() {
+    console.log(`Library name: ${this.name}`);
+    console.log(`Total books: ${this.books.length}`);
+    console.log(`Available: ${this.listAvailableBooks().length}`);
+    console.log(`Borrowed: ${this.listBorrowedBooks().length}`);
+  }
 }
